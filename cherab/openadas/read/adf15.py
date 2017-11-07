6295c7bca8a4acecc2b43aa4f7479c40a725e278
Matthew Carr <MatthewNCarr@gmail.com>
2017-11-07 09:35:52 +0000

    Some improvements to the way ADF15 files are handled. Now takes a more advanced transition identification string. Added ability to auto-download default files. Towards #8.

--- cherab/openadas/read/adf15.py
+++ cherab/openadas/read/adf15.py
@@ -15,17 +15,127 @@
 # under the Licence.
 
 
-import os
 import re
-
 import numpy as np
+from cherab.core.atomic import hydrogen
 
-# from cherab.openadas.rates import PEC
 
-PEC_INDEX_HEADER_MATCH = '^C\s*[A-Z]*\s*[A-Z]*\s*[A-Z]*\s*[A-Z]*$'
-PEC_TRANSITION_MATCH = '^C\s*([0-9]*)\.\s*([0-9]*\.[0-9])\s*N=\s*([0-9]*).*N=\s*([0-9]*)\s*([A-Z]*)$'
+PEC_INDEX_HEADER_MATCH = '^C\s*ISEL\s*WAVELENGTH\s*TRANSITION\s*TYPE'
+CONFIGURATION_HEADER_MATCH = '^C\s*Configuration\s*\(2S\+1\)L\(w-1/2\)\s*Energy \(cm\*\*-1\)$'
+
+CONFIGURATION_STRING_MATCH = "^C\s*([0-9]*)\s*((?:[0-9][SPDFG][0-9]\s)*)\s*\(([0-9]*\.?[0-9]*)\)([0-9]*)\(\s*([0-9]*\.?[0-9]*)\)"
+
+PEC_N_TRANSITION_MATCH = '^C\s*([0-9]*)\.\s*([0-9]*\.[0-9])\s*N=\s*([0-9]*).*N=\s*([0-9]*)\s*([A-Z]*)$'
+PEC_HYDROGEN_TRANSITION_MATCH = '^\s*([0-9]*)\.\s*([0-9]*\.[0-9]*)\s*N=\s*([0-9]*) - N=\s*([0-9]*)\s*([A-Z]*)'
+PEC_FULL_TRANSITION_MATCH = '^C\s*([0-9]*)\.\s*([0-9]*\.[0-9])\s*([0-9]*)[\(\)\.0-9\s]*-\s*([0-9]*)[\(\)\.0-9\s]*([A-Z]*)'
 WAVELENGTH_MATCH = '^\s*[0-9]*\.[0-9] ?A .*$'
 BLOCK_ID_MATCH = '^\s*[0-9]*\.[0-9] ?A\s*([0-9]*)\s*([0-9]*).*/TYPE = ([a-zA-Z]*).*/ISEL *= * ([0-9]*)$'
+
+_L_LOOKUP = {
+    0: 'S',
+    1: 'P',
+    2: 'D',
+    3: 'F',
+    4: 'G',
+    5: 'H',
+    6: 'I',
+    7: 'K',
+    8: 'L',
+    9: 'M',
+    10: 'N',
+    11: 'O',
+    12: 'Q',
+    13: 'R',
+}
+
+
+def add_adf15_to_atomic_data(atomic_data_dictionary, element, ionisation, adf_file_path):
+
+    adf15_file = open(adf_file_path, "r")
+    lines = adf15_file.readlines()
+
+    # Use simple electron configuration structure for hydrogen
+    if element == hydrogen:
+
+        while not re.match(PEC_INDEX_HEADER_MATCH, lines[0]):
+            lines.pop(0)
+        index_lines = lines
+
+        for i in range(len(index_lines)):
+
+            match = re.match(PEC_HYDROGEN_TRANSITION_MATCH, index_lines[i])
+            if not match:
+                continue
+
+            block_num = int(match.groups()[0])
+            wavelength = float(match.groups()[1])/10
+            upper_level = int(match.groups()[2])
+            lower_level = int(match.groups()[3])
+            rate_type_adas = match.groups()[4]
+            if rate_type_adas == 'EXCIT':
+                rate_type = 'excitation'
+            elif rate_type_adas == 'RECOM':
+                rate_type = 'recombination'
+            elif rate_type_adas == 'CHEXC':
+                rate_type = 'cx_thermal'
+            else:
+                raise ValueError("Unrecognised rate type - {}".format(rate_type_adas))
+
+            atomic_data_dictionary[rate_type][element][ionisation][(upper_level, lower_level)] = (adf_file_path, block_num)
+            atomic_data_dictionary["wavelength"][element][ionisation][(upper_level, lower_level)] = wavelength
+
+    # Use full electron configuration structure for anything else
+    else:
+
+        configuration_lines = []
+        configuration_dict = {}
+
+        while not re.match(CONFIGURATION_HEADER_MATCH, lines[0]):
+            lines.pop(0)
+        while not re.match(PEC_INDEX_HEADER_MATCH, lines[0]):
+            configuration_lines.append(lines[0])
+            lines.pop(0)
+        index_lines = lines
+
+        for i in range(len(configuration_lines)):
+
+            match = re.match(CONFIGURATION_STRING_MATCH, configuration_lines[i])
+            if not match:
+                continue
+
+            config_id = int(match.groups()[0])
+            electron_configuration = match.groups()[1].rstrip().lower()
+            spin_multiplicity = match.groups()[2]  # (2S+1)
+            total_orbital_quantum_number = _L_LOOKUP[int(match.groups()[3])]  # L
+            total_angular_momentum_quantum_number = match.groups()[4]  # J
+
+            configuration_dict[config_id] = (electron_configuration + " " + spin_multiplicity +
+                                             total_orbital_quantum_number + total_angular_momentum_quantum_number)
+
+        for i in range(len(index_lines)):
+
+            match = re.match(PEC_FULL_TRANSITION_MATCH, index_lines[i])
+            if not match:
+                continue
+
+            block_num = int(match.groups()[0])
+            wavelength = float(match.groups()[1])/10
+            upper_level_id = int(match.groups()[2])
+            upper_level = configuration_dict[upper_level_id]
+            lower_level_id = int(match.groups()[3])
+            lower_level = configuration_dict[lower_level_id]
+            rate_type_adas = match.groups()[4]
+            if rate_type_adas == 'EXCIT':
+                rate_type = 'excitation'
+            elif rate_type_adas == 'RECOM':
+                rate_type = 'recombination'
+            elif rate_type_adas == 'CHEXC':
+                rate_type = 'cx_thermal'
+            else:
+                raise ValueError("Unrecognised rate type - {}".format(rate_type_adas))
+
+            atomic_data_dictionary[rate_type][element][ionisation][(upper_level, lower_level)] = (adf_file_path, block_num)
+            atomic_data_dictionary["wavelength"][element][ionisation][(upper_level, lower_level)] = wavelength
 
 
 # Group lines of file into blocks based on precursor '  6561.9A   24...'
@@ -99,5 +209,3 @@ def adf15(file_name, block_num):
 
         # If code gets to here, block wasn't found.
         raise RuntimeError('Block number {} was not found in ADF15 file {}.'.format(block_num, file_name))
-
-
